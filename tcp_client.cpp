0b9cc0624053287837257fc631395daeecd02a6f
matrixx17 <76977221+matrixx17@users.noreply.github.com>
2025-07-30 21:47:27 -0400

    initial implementation of client

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -1,33 +1,44 @@
-#include <stdio>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <errno.h>
-// add rest
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
 
 int main() {
-	int fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd < 0) {
-		die("socket()");
-	}
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
 
-	struct sockaddr_in addr = {};
-	addr.sin_family = AF_NET;
-	addr.sin_port = ntohs(1234);
-	addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
-	int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-	if (rv) {
-		die("connect()");
-	}
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
+    }
 
-	char msg[] = "hello";
-	write(fd, msg, strlen(msg));
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
 
-	char rbuf[64] = {};
-	size_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-	if (n < 0) {
-		die("read()");
-	}
-	printf("server: %s\n", rbuf);
-	close(fd);
-}
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        die("read");
+    }
+    printf("server says: %s\n", rbuf);
+    close(fd);
+    return 0;
 }
